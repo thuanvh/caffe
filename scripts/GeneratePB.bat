5547792dffaddf7d6bff5c99724ebc6ab97a42c2
Vu Hong Thuan <thuanvh@gmail.com>
2015-10-26 13:02:06 +0700

    Copy caffe to include folder.

--- scripts/GeneratePB.bat
+++ scripts/GeneratePB.bat
@@ -4,5 +4,7 @@ rem ) else (
 echo caffe.pb.h is being generated
 "./tools/protoc" -I="./src/caffe/proto" --cpp_out=dllexport_decl=CAFFE_DLL_EXPORT:"./src/caffe/proto" "./src/caffe/proto/caffe.proto"
 "./tools/protoc" -I="./src/caffe/proto" --cpp_out=dllexport_decl=CAFFE_DLL_EXPORT:"./src/caffe/proto" "./src/caffe/proto/caffe_pretty_print.proto"
+md "./include/caffe/proto/"
+move ".\src\caffe\proto\caffe.pb.h" ".\include\caffe\proto\"
 rem )
 rem  protoc --cpp_out=dllexport_decl=MY_EXPORT_MACRO:path/to/output/dir myproto.proto
